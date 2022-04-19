b4897084de3d99521c2198567f9c2992be8a5096
Kieran <98961254+XLV150@users.noreply.github.com>
2022-04-19 21:18:27 +0200

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,89 @@
+import sqlite3
+
+print("1. Log into an Existing account. \n2. Create a account. \n3. Delete an existing account.")
+user_choice = int(input("->"))
+
+db = sqlite3.connect("ui.sqlite")
+cursor = db.cursor()
+
+db.execute("CREATE TABLE IF NOT EXISTS ui (Username TEXT, Password TEXT)")
+db.commit()
+
+if user_choice == 1:
+
+    user1 = input("Enter a Username: ")
+    user2 = input("Enter a Password: ")
+
+    print()
+
+    cursor.execute("SELECT Username from ui where Username = '{}' LIMIT 1".format(user1))
+    if cursor.fetchone():
+        print("Username Success")
+    else:
+        print("Username Fail")
+    cursor.execute("SELECT Password from ui where Password = '{}' LIMIT 1".format(user2))
+    if cursor.fetchone():
+        print("Password Success")
+    else:
+        print("Password Fail")
+    print()
+
+elif user_choice == 2:
+
+    user3 = input("Enter your new username: ")
+    user4 = input("Enter your new password: ")
+    print()
+
+    cursor.execute("SELECT Username from ui where Username = '{}' LIMIT 1".format(user3))
+    if cursor.fetchone():
+        print("Username Already exists. Try another one.")
+        print()
+    else:
+        print("Username Available")
+        cursor.execute("SELECT Password from ui where Password = '{}' LIMIT 1".format(user4))
+        if cursor.fetchone():
+            print("Oops there was an error enter another Password!")
+            print()
+        else:
+            print("Password Success")
+            db.execute("INSERT INTO ui(Username, Password) VALUES('{}', '{}')".format(user3, user4))
+            db.commit()
+            print()
+            print("You have successfully created a new Account!")
+            print()
+
+
+elif user_choice == 3:
+    print("In order to delete your account we have to confirm its you. Enter your Username and Password"
+          " and your account will be deleted.")
+    print()
+    user5 = input("Enter your Username: ")
+    user6 = input("Enter your Password: ")
+    print()
+
+    cursor.execute("SELECT Username from ui where Username = '{}' LIMIT 1".format(user5))
+    if cursor.fetchone():
+        print("Username Success.")
+        cursor.execute("SELECT Password from ui where Password = '{}' LIMIT 1".format(user6))
+        if cursor.fetchone():
+            print("Password Success. \nDeleting Account now")
+            db.execute("DELETE FROM ui WHERE Username = '{}'".format(user5))
+            db.execute("DELETE FROM ui WHERE Password = '{}'".format(user6))
+            db.commit()
+            print()
+        else:
+            print("Password does not exist or was entered incorrectly.")
+            print()
+    else:
+        print("Username does not exist, or was entered incorrectly.")
+        print()
+
+print("SHOWING ALL ACCOUNTS NOW")
+cursor.execute("SELECT * FROM ui")
+for Username, Password in cursor:
+    print(Username)
+    print(Password)
+    print("-" * 20)
+
+cursor.close()
+db.close()
